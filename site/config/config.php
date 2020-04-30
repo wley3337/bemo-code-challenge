21aa951a4d59d7f56e67736624ed7f97afdb4db6
wley3337 <wley3337@gmail.com>
2020-04-30 08:17:46 -0400

    trouble shooting email'

--- site/config/config.php
+++ site/config/config.php
@@ -3,5 +3,15 @@ return [
   'panel' => [
     'install' => true
   ],
-  'debug' => true
+  'debug' => true,
+  'email' => [
+    'presets' => [
+      'contact' => [
+        'from'    => 'no-reply@supercompany.com',
+        'subject' => 'Thank you for your contact request',
+        'cc'      => 'wley3337@gmail.com',
+        'body'    => 'We will never reply'
+      ]
+    ]
+  ]
 ];
